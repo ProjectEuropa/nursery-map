d5797c0f4c92bd162e6796d8a20aa5c61e7f87fd
宮本雅人 <masato@miyamotomasatonoMacBook-puro.local>
2018-08-13 20:37:48 +0900

    feat: indexメソッドを削除

--- app/main.py
+++ app/main.py
@@ -4,10 +4,6 @@ from flask_cors import CORS
 app = Flask(__name__, static_url_path='')
 cors = CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:3000"}})
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/<string:page_name>/')
 def render_static(page_name):
     return render_template('%s.html' % page_name)
