c2ad925a6efc474012d855f81abbc25df24891c0
宮本雅人 <masato@miyamotomasatonoMacBook-puro.local>
2018-08-13 18:34:11 +0900

    feat: distへの移動を手動ではなくDockerfileに記述

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, Blueprint
 from flask_cors import CORS
 
-app = Flask(__name__, static_url_path=None)
+app = Flask(__name__, static_url_path='')
 cors = CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:3000"}})
+
 
 @app.route('/<string:page_name>/')
 def render_static(page_name):
     return render_template('%s.html' % page_name)
+
+@app.route('/<string:dir_name>/index')
+def render_static_dir(dir_name):
+    return render_template(dir_name + '/index.html')
 
 @app.route('/api')
 def api():
@@ -20,8 +25,6 @@ def api():
   return jsonify(data)
 
 if __name__ == '__main__':
-    app.static_url_path=app.config.get('/')
-    app.static_folder='/'
-    print(app.static_url_path)
-    print(app.static_folder) 
+    app.static_url_path='/'
+    app.static_folder=app.root_path
     app.run(host="0.0.0.0", port=80, debug=True)
